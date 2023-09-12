b9faa3378d41e228814c56cf0d62f540886b3f2f
Iga Ryś <imirys96@gmail.com>
2023-09-12 12:50:22 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -5,22 +5,19 @@ export const ContactList = ({ contacts , deleteContacts}) => {
   return (
     <ul style={{ display: 'inline-flex', flexDirection: 'column', 'lineHeight': '1.3em', 'gap': '5px' }}>
       {contacts.map(contact => (
-        <div
-          style={{ display: 'inline-flex', justifyContent: 'space-between' }}
-        >
           <li style={{ display: 'list-item', 'listStyleType': 'circle' }}
             key={contact.id}>
             {contact.name} : {contact.number}
-          </li>
           <button
-            className={css.Button}
-            onClick={ () => deleteContacts(contact.id)}
-            type="button">
+          className={css.Button}
+          onClick={ () => deleteContacts(contact.id)}
+          type="button">
             Delete
           </button>
-        </div>
+          </li>
       ))}
     </ul>
+  
   );
 };
 
